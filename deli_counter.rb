4abdcc56345ca5cbbcebfc01f3d36871432a7558
Ahmed Gaber <21stlimitless@gmail.com>
2019-05-30 02:02:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,7 @@ end
 
 def now_serving(array)
   if array.length == 0
-    puts "The line is empty."
+    puts "There is nobody waiting to be served!"
   elsif array.length > 0
     puts "Currently serving #{array.shift}"
   end
