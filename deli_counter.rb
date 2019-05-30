81292099d7a26416084fcd825ecbcf49bbae5232
Ahmed Gaber <21stlimitless@gmail.com>
2019-05-30 01:12:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+katz_deli = []
+
+def line(array)
+  
+  if array.length == 0
+    puts "The line is currently empty."
+  else for index in 0...array.size
+    puts "The line is currently: #{array[index]}. #{array}"
+    end
+  end
+  
+end
