4107e61a149cbcc2f61d523a22421a428eb3088c
Ahmed Gaber <21stlimitless@gmail.com>
2019-05-30 01:24:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,7 @@ def line(array)
   
   if array.length == 0
     puts "The line is currently empty."
-  else for index in 0...array.size
-    puts "The line is currently: #{array[index]}. #{array}"
+  else array.collect { |n| "The line is currently: #{n}"
     end
   end
   
