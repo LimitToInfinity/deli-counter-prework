c7c22938c56e71a22cbbaf1fda00d6218498f93f
Ahmed Gaber <21stlimitless@gmail.com>
2019-05-30 01:57:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,6 +13,7 @@ def line(array)
 end
 
 def take_a_number(array, name)
+  
   if array.length == 0
     array.push(name)
     puts "Welcome, #{name}. You are number #{array.length} in line."
@@ -20,4 +21,5 @@ def take_a_number(array, name)
     array.push(name)
     puts "Welcome, #{name}. You are number #{array.length} in line."
   end
+
 end
